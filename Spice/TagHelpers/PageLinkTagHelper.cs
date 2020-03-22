dc5f8d919ecbdf9a191548ae7cfc17be15ea8ba7
Yhenrymcbean <henry_mcbean@hotmail.com>
2020-03-22 22:11:27 +0000

    Completed 12. PageLinkTagHelper.

---
+++ Spice/TagHelpers/PageLinkTagHelper.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Spice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spice.TagHelpers
+{
+    [HtmlTargetElement("DIV", Attributes = "page-model")]
+    public class PageLinkTagHelper : TagHelper
+    {
+        private IUrlHelperFactory _urlHelperFactory;
+
+        public PageLinkTagHelper(IUrlHelperFactory urlHelperFactory)
+        {
+            _urlHelperFactory = urlHelperFactory;
+        }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
+
+        public PagingInfo PageModel { get; set; }
+        public string PageAction { get; set; }
+        public bool PageClassesEnabled { get; set; }
+        public string PageClass { get; set; }
+        public string PageClassNormal { get; set; }
+        public string PageClassSelected { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            IUrlHelper urlHelper = _urlHelperFactory.GetUrlHelper(ViewContext);
+            TagBuilder ressult = new TagBuilder("DIV");
+
+            for (int i = 0; i <= PageModel.totalPage; i++)
+            {
+                TagBuilder tag = new TagBuilder("a");
+                string url = PageModel.urlParam.Replace(":", i.ToString());
+                tag.Attributes["href"] = url;
+
+                if (PageClassesEnabled)
+                {
+                    tag.AddCssClass(PageClass);
+                    tag.AddCssClass(i == PageModel.CurrentPage ? PageClassSelected : PageClassNormal);
+                }
+                tag.InnerHtml.Append(i.ToString());
+                ressult.InnerHtml.AppendHtml(tag);
+            }
+
+            output.Content.AppendHtml(ressult.InnerHtml);
+        }
+    }
+}
