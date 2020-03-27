2de13725a68ad54d3e8e6bb52d74c8ed1ae7f4e6
Yhenrymcbean <henry_mcbean@hotmail.com>
2020-03-27 02:06:32 +0000

    Completed 7. Roles Login Check.

--- Spice/Startup.cs
+++ Spice/Startup.cs
@@ -33,7 +33,7 @@ namespace Spice
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
 
-            services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddDefaultTokenProviders()
                 .AddDefaultUI()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
